fd97036e93ef7b101e58db719265e06f02479da1
skhasan536 <hasan.shaikh@wybrid.com>
2025-08-23 19:17:29 +0530

    Refactor: Move device_preview to dev_dependencies and add flutter_native_splash
    This commit moves the `device_preview` package from main dependencies to `dev_dependencies` in `pubspec.yaml`.
    
    Additionally, the `flutter_native_splash` package has been added as a dev dependency. This also involved updates to the `.flutter-plugins-dependencies` and `pubspec.lock` files to reflect the new dependency and its transitive dependencies (ansicolor, archive, crypto, csslib, html, image, posix, universal_io, yaml).
    
    The `router.dart` file had a minor formatting change (trailing comma removal). The commented-out `ViewStateController` class in `view_state_controller.dart` was also removed.

--- lib/core/common/view_state_controller.dart
+++ lib/core/common/view_state_controller.dart
@@ -1,20 +1,6 @@
 import 'package:flutter/cupertino.dart';
 
 enum ViewState { idle, busy, complete }
-//
-// class ViewStateController extends GetxController {
-//   final _viewState = ViewState.idle.obs;
-//
-//   ViewState get viewState => _viewState.value;
-//   bool get isLoading => _viewState.value == ViewState.busy;
-//
-//   void setViewState(ViewState state, {bool notify = true}) {
-//     _viewState.value = state;
-//     if (notify) {
-//       update();
-//     }
-//   }
-// }
 
 class ViewStateProvider extends ChangeNotifier {
   ViewState _viewState = ViewState.complete;
