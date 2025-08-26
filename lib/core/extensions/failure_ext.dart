e70a811ea6be1d12d01c610a7c0aa049266cc367
skhasan536 <hasan.shaikh@wybrid.com>
2025-08-26 15:45:39 +0530

    Refactor: Improve Toast Message Display with Custom Overlay
    This commit refactors the toast message functionality.
    
    The existing `Toast` class, which utilized `ScaffoldMessenger.showSnackBar`, has been renamed to `Toasts`.
    
    The new `Toasts` class now implements a custom overlay-based toast system. This provides more control over the appearance and animation of toast messages.
    
    Key changes include:
    - Renamed `Toast` to `Toasts` and updated its usage in `FailureExt`.
    - Implemented `_showToast`, a private method that creates and manages an `OverlayEntry` for displaying toast messages.
    - Added animation using `AnimationController` and `SlideTransition` for a smooth appearance/disappearance.
    - Toast messages now appear at the top of the screen, below the status bar.
    - Introduced distinct styling for info, error, and success toasts, including different background colors, border colors, and icons.
    - Added `showSuccessOrFailureToast` utility method to display appropriate toast messages based on the success or failure of an operation, with optional callbacks and navigation actions.
    - Existing methods `showErrorToast`, `showSuccessToast`, and `showInfoToast` have been updated to use the new `_showToast` implementation.
    - Ensured only one toast can be displayed at a time by managing `currEntry`.
    - Toasts automatically dismiss after 4 seconds.

--- lib/core/extensions/failure_ext.dart
+++ lib/core/extensions/failure_ext.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart' show BuildContext;
 import 'package:tc_sa/core/network/app_failure.dart' show Failure;
-import 'package:tc_sa/core/utils/toast.dart' show Toast;
+import 'package:tc_sa/core/utils/toast.dart' show Toasts;
 
 extension FailureExt on Failure {
   bool get isExists => message != null && statusCode != null;
 
   void showError(BuildContext context) {
     if (statusCode != 500) {
-      Toast.showErrorToast(context, message: message ?? 'Something Went Wrong');
+      Toasts.showErrorToast(
+        context,
+        message: message ?? 'Something Went Wrong',
+      );
     } else {
       print('Issue found: $message with $statusCode');
     }
--- lib/core/utils/toast.dart
+++ lib/core/utils/toast.dart
@@ -1,36 +1,101 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:tc_sa/common/index.dart' show STextStyles;
+import 'package:tc_sa/core/index.dart';
 
-class Toast {
-  static void showErrorToast(BuildContext context, {required String message}) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Icon(Icons.error_outline, color: Colors.white),
-            SizedBox(width: 12),
-            Expanded(
-              child: Text(
-                message,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w600,
-                  fontSize: 18,
+class Toasts {
+  static OverlayEntry? currEntry;
+
+  static void _showToast(
+    BuildContext context, {
+    required String message,
+    required Color backgroundClr,
+    required Border border,
+    required IconData icon,
+  }) {
+    final overlay = Overlay.of(context); // just
+
+    if (currEntry != null) return;
+
+    // Animation controller
+    AnimationController controller = AnimationController(
+      vsync: Navigator.of(context),
+      duration: Duration(milliseconds: 300),
+    );
+
+    final overlayEntry = OverlayEntry(
+      builder: (context) {
+        return Positioned(
+          top: MediaQuery.of(context).padding.top, // below status bar
+          left: 16,
+          right: 16,
+          child: Material(
+            color: Colors.transparent,
+            child: SlideTransition(
+              position: Tween<Offset>(
+                begin: Offset(0, -1),
+                end: Offset(0, 0),
+              ).animate(
+                CurvedAnimation(parent: controller, curve: Curves.easeOut),
+              ),
+              child: Container(
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: backgroundClr,
+                  borderRadius: BorderRadius.circular(8),
+                  border: border,
                 ),
-                maxLines: 2,
-                overflow: TextOverflow.visible,
-                softWrap: true,
+                child: Row(
+                  children: [
+                    Icon(icon, size: 32, color: Colors.black),
+                    SizedBox(width: 12),
+                    Expanded(
+                      child: Text(
+                        message,
+                        style: STextStyles.s14W400.copyWith(
+                          color: Colors.black,
+                        ),
+                        maxLines: 2,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ],
-        ),
-        behavior: SnackBarBehavior.floating,
-        duration: Duration(seconds: 3),
-        backgroundColor: Colors.red,
-        showCloseIcon: true,
-        closeIconColor: Colors.black,
-      ),
+          ),
+        );
+      },
+    );
+
+    overlay.insert(overlayEntry);
+    currEntry = overlayEntry;
+    controller.forward();
+
+    Future.delayed(Duration(seconds: 4), () {
+      controller.reverse().then((_) {
+        currEntry = null;
+        overlayEntry.remove();
+      });
+    });
+  }
+
+  static void showInfoToast(BuildContext context, {required String message}) {
+    _showToast(
+      context,
+      message: message,
+      backgroundClr: Colors.yellow.shade100,
+      border: Border.all(color: Colors.orange, width: 1.5),
+      icon: Icons.info_outline,
+    );
+  }
+
+  static void showErrorToast(BuildContext context, {required String message}) {
+    _showToast(
+      context,
+      message: message,
+      backgroundClr: Colors.pink.shade100,
+      border: Border.all(color: Colors.red, width: 2),
+      icon: Icons.error_outline,
     );
   }
 
@@ -38,68 +103,51 @@ class Toast {
     BuildContext context, {
     required String message,
   }) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Icon(Icons.error_outline, color: Colors.white),
-            SizedBox(width: 12),
-            Expanded(
-              child: Text(
-                message,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w600,
-                  fontSize: 18,
-                ),
-                maxLines: 2,
-                overflow: TextOverflow.visible,
-                softWrap: true,
-              ),
-            ),
-          ],
-        ),
-        behavior: SnackBarBehavior.floating,
-        duration: Duration(seconds: 3),
-        backgroundColor: Colors.green,
-        showCloseIcon: true,
-        closeIconColor: Colors.white,
-      ),
+    _showToast(
+      context,
+      message: message,
+      backgroundClr: Colors.green.shade100,
+      border: Border.all(color: Colors.green.shade700, width: 2),
+      icon: Icons.check,
     );
   }
 
-  static void showInfoToast(BuildContext context, {required String message}) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Icon(Icons.error_outline, color: Colors.black),
-            SizedBox(width: 12),
-            Expanded(
-              child: Text(
-                message,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.w600,
-                  fontSize: 18,
-                ),
-                maxLines: 2,
-                overflow: TextOverflow.visible,
-                softWrap: true,
-              ),
-            ),
-          ],
-        ),
-        behavior: SnackBarBehavior.floating,
-        duration: Duration(seconds: 3),
-        backgroundColor: Colors.yellow,
-        showCloseIcon: true,
-        closeIconColor: Colors.black,
-      ),
-    );
+  static void showSuccessOrFailureToast(
+    BuildContext context, {
+    Failure? failure,
+    String? failureMsg,
+    VoidCallback? failureCallback,
+    bool popOnFailure = false,
+    bool popOnSuccess = true,
+    String? successMsg,
+    VoidCallback? successCallback,
+  }) {
+    if (failure == null) {
+      Toasts.showSuccessToast(
+        context,
+        message: successMsg ?? 'Api Hit Successfully',
+      );
+
+      if (successCallback != null) {
+        successCallback();
+      }
+
+      if (popOnSuccess) {
+        context.pop(true);
+      }
+    } else {
+      Toasts.showErrorToast(
+        context,
+        message: failureMsg ?? (failure.message ?? 'API hit Failed'),
+      );
+
+      if (failureCallback != null) {
+        failureCallback();
+      }
+
+      if (popOnFailure) {
+        context.pop(true);
+      }
+    }
   }
 }
